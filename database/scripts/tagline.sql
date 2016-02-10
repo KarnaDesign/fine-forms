ce49716094f5e625c808cd5fc346d4cc64762d3c
Ida Ferm <ida.kg.ferm@gmail.com>
2016-02-10 19:56:30 +0100

    Added a script for creating the tagline table
    Also added inserts for default taglines.

---
+++ database/scripts/tagline.sql
@@ -0,0 +1,14 @@
+CREATE TABLE tagLine (
+  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  quote VARCHAR(255) NOT NULL
+);
+
+INSERT INTO tagLine (quote) VALUES ('Don\'t Blink');
+INSERT INTO tagLine (quote) VALUES ('Silence Will Fall');
+INSERT INTO tagLine (quote) VALUES ('Bow ties Are Cool');
+INSERT INTO tagLine (quote) VALUES ('Time Can Be Rewritten');
+INSERT INTO tagLine (quote) VALUES ('Geronimo!');
+INSERT INTO tagLine (quote) VALUES ('Come Along Pond');
+INSERT INTO tagLine (quote) VALUES ('Fixed Point In Time');
+INSERT INTO tagLine (quote) VALUES ('A big ball of wibbly wobbly... time-y wimey... stuff');
+INSERT INTO tagLine (quote) VALUES ('Air From My Lungs');
