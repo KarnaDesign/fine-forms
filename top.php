51fc984cc7679654540e65d8a93e11fcda5e5e0c
Ida Ferm <ida.kg.ferm@gmail.com>
2016-02-14 19:19:53 +0100

    Forgot the actual link to the new stylesheet

--- top.php
+++ top.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device, initial-scale=1.0">
     <link href='https://fonts.googleapis.com/css?family=Slabo+27px' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="style/formhandler_style.css">
 </head>
 
 <body>
