ee027c0807276f4d5254ff27b21fbc9d1e150afb
Ida Ferm <ida.kg.ferm@gmail.com>
2016-02-20 19:32:36 +0100

    Created animated notification and return paths

--- js/form.js
+++ js/form.js
@@ -1,4 +1,29 @@
+function centerNotification()
+{
+    var wrapperWidth = $("#wrapper").width();
+    var notificationWidth = $(".notification").width();
+
+    var halfWrapperWidth = wrapperWidth / 2;
+    var halfNotificationWidth = notificationWidth / 2;
+    var newPosition = halfWrapperWidth - halfNotificationWidth;
+
+    $(".notification").css("left", newPosition + "px");
+}
+
+function hideNotification()
+{
+    var delayInMilliseconds = 10000;
+    setTimeout(function(){
+        $(".notification").fadeOut();
+    }, delayInMilliseconds);
+}
+
+
 $(document).ready(function()
 {
-    $("#applicationForm").reset(); //Resets the form after refreshing the page. Does not apply to Firefox.
+    $("#applicationForm")[0].reset(); //Resets the form after refreshing the page. Does not apply to Firefox.
+
+    centerNotification();
+
+    hideNotification();
 });
